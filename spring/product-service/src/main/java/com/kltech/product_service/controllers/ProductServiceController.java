97e1ceb4f2539bad78b38a856dcab5bb958ab559
Thang Loi Tran <tranthangloi.dev@gmail.com>
2025-03-25 23:22:28 +0700

    feat: product-service with docker-compose

---
+++ spring/product-service/src/main/java/com/kltech/product_service/controllers/ProductServiceController.java
@@ -0,0 +1,23 @@
+package com.kltech.product_service.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kltech.product_service.models.response.BaseResponse;
+
+
+@RestController
+public class ProductServiceController {
+    @GetMapping("/products")
+    public ResponseEntity<BaseResponse> getProducts() {
+        return ResponseEntity
+            .ok()
+            .body(BaseResponse.builder()
+                .message("Get All Products Successfully")
+                .status(true)
+                .statusCode(200)
+                .build());
+    }
+    
+}
