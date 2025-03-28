e1b09ce7cf6dfd514e233a14e49137163cd6d32a
Thang Loi Tran <tranthangloi.dev@gmail.com>
2025-03-28 23:22:02 +0700

    feat: api gateway

--- spring/product-service/src/main/java/com/kltech/product_service/controllers/ProductServiceController.java
+++ spring/product-service/src/main/java/com/kltech/product_service/controllers/ProductServiceController.java
@@ -2,14 +2,16 @@ package com.kltech.product_service.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kltech.product_service.models.response.BaseResponse;
 
 
 @RestController
+@RequestMapping("/api/v1/products")
 public class ProductServiceController {
-    @GetMapping("/products")
+    @GetMapping("/")
     public ResponseEntity<BaseResponse> getProducts() {
         return ResponseEntity
             .ok()
